6ae2e4036e6a16a9f2df4fe07435be873a07ce13
lequocviet401 <khuubao123456@gmail.com>
2025-02-21 15:53:57 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u=username.value
+  let p=pw.value
+  if(u=="qvit" && p=="qvitiuga")
+  {
+    window.open("home.html")
+    document.getElementById(compiler).style.display="block"
+  }
+}
